53ad3fa2708218ad803d24f9303b022031f38153
Juan Sebastián Hernández Cárdenas <57507306+sudorootuser@users.noreply.github.com>
2022-10-11 21:04:16 -0500

    Primer commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'slides',
+    pathMatch: 'full'
+  },
+  {
+    path: '',
+    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'tab4',
+    loadChildren: () => import('./tab4/tab4.module').then( m => m.Tab4PageModule)
+  },
+  {
+    path: 'slides',
+    loadChildren: () => import('./slides/slides.module').then( m => m.SlidesPageModule)
+  }
+];
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
---
+++ src/app/tab3/tab3.page.ts
@@ -0,0 +1,65 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tab3',
+  templateUrl: 'tab3.page.html',
+  styleUrls: ['tab3.page.scss']
+})
+export class Tab3Page {
+
+  // NHW
+
+  nhw4="https://guaudog.github.io/img/shw/shw1.png";
+  nhw3="https://guaudog.github.io/img/shw/shw2.png";
+  nhw2="https://guaudog.github.io/img/shw/shw3.png";
+  nhw1="https://guaudog.github.io/img/shw/shw4.png";
+
+
+
+  //Pasion deportiva
+
+  deporte1="https://guaudog.github.io/img/pasionDeportiva/dashboard.png";
+  deporte2="https://guaudog.github.io/img/pasionDeportiva/deporte1.png";
+  deporte3="https://guaudog.github.io/img/pasionDeportiva/deporte2.png";
+  deporte4="https://guaudog.github.io/img/pasionDeportiva/deporte3.png";
+  deporte5="https://guaudog.github.io/img/pasionDeportiva/errorInicio.png";
+  deporte6="https://guaudog.github.io/img/pasionDeportiva/error_usuario.png";
+
+  
+
+  
+  // Volando
+
+  volando1="https://guaudog.github.io/img/volando/volando1.png";
+  volando2="https://guaudog.github.io/img/volando/volando2.png";
+  volando3="https://guaudog.github.io/img/volando/volando3.png";
+
+
+  
+  // Farma day
+
+  farma1="https://guaudog.github.io/img/farmaday/Farma_Dasboard.png";
+  farma5="https://guaudog.github.io/img/farmaday/Farma_Inicio.png";
+  farma3="https://guaudog.github.io/img/farmaday/Tec_Ventas_reg.png";
+  farma4="https://guaudog.github.io/img/farmaday/Farma_Registro.png";
+  farma2="https://guaudog.github.io/img/farmaday/Farma_gest_user.png";
+
+
+  
+  
+  // OC
+
+  oc1="https://guaudog.github.io/img/oc/oc1.png";
+  oc2="https://guaudog.github.io/img/oc/oc2.png";
+  oc3="https://guaudog.github.io/img/oc/oc3.png";
+  oc4="https://guaudog.github.io/img/oc/oc4.png";
+  oc5="https://guaudog.github.io/img/oc/ocweb.png";
+  oc6="https://guaudog.github.io/img/oc/ocweb2.png";
+
+
+  constructor() { }
+
+
+
+
+}
